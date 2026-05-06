2ad5261a72113296c63b2fc1a521f74b242426d7
tenbj <141602625+tenbj@users.noreply.github.com>
2026-05-07 00:40:57 +0800

    Delete .temp directory

--- .temp/fix_bom2.ps1
+++
@@ -1,7 +0,0 @@
-﻿ = Get-ChildItem -LiteralPath '.agents\skills记忆管理\scripts' -Filter '*.ps1'
-foreach ( in ) {
-     = .FullName
-     = [System.IO.File]::ReadAllText()
-    [System.IO.File]::WriteAllText(, , [System.Text.UTF8Encoding]::new(True))
-    Write-Host ('BOM fixed: ' + )
-}
--- .temp/fix_bom3.ps1
+++
@@ -1,8 +0,0 @@
-﻿param()
-$skillPath = Join-Path (Get-Location) ".agents\skills\记忆管理\scripts"
-Get-ChildItem -LiteralPath $skillPath -Filter "*.ps1" | ForEach-Object {
-    $p = $_.FullName
-    $c = [System.IO.File]::ReadAllText($p)
-    [System.IO.File]::WriteAllText($p, $c, [System.Text.UTF8Encoding]::new($true))
-    Write-Host ("BOM fixed: " + $_.Name)
-}
